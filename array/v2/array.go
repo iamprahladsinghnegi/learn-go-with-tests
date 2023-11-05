e852f7e25a12591a032f5a2d913b69602708eef3
iamprahladsinghengi <mecoolpankajnegi@gmail.com>
2023-11-05 16:20:58 +0530

    feat: generics with arrays/slices

---
+++ array/v2/array.go
@@ -0,0 +1,26 @@
+package array
+
+func Sum(numbers []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	addAll := func(acc, x []int) []int {
+		return append(acc, Sum(x))
+	}
+	return Reduce(numbersToSum, addAll, []int{})
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
+	}
+
+	return Reduce(numbersToSum, sumTail, []int{})
+}
