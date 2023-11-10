d941fa21c28d1ac4ffdba03ebe55465533d9d9da
iamprahladsinghengi <mecoolpankajnegi@gmail.com>
2023-11-11 01:37:18 +0530

    feat: cli

---
+++ cli/cmd/cli/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	poker "learn-go-with-tests/cli"
+
+	"fmt"
+	"log"
+	"os"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+	poker.NewCLI(store, os.Stdin).PlayPoker()
+}
